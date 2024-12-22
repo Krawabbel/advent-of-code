1b4d21729114f025d43780531ca545122ec5959c
Dr. Krawabbel <drkrawabbel@gmail.de>
2024-12-23 00:59:21 +0100

    2024 day 21 solved :)

--- 2024/21-keypad-conundrum/main.go
+++ 2024/21-keypad-conundrum/main.go
@@ -61,7 +61,7 @@ const (
 	PAD_9     = '9'
 )
 
-var POS_NUMERIC = map[byte]Point{
+var POS_NUMERICAL = map[byte]Point{
 	PAD_7: {0, 0},
 	PAD_8: {0, 1},
 	PAD_9: {0, 2},
@@ -75,6 +75,28 @@ var POS_NUMERIC = map[byte]Point{
 	PAD_A: {3, 2},
 }
 
+var POS_DIRECTIONAL = map[byte]Point{
+	PAD_UP:    {0, 1},
+	PAD_A:     {0, 2},
+	PAD_LEFT:  {1, 0},
+	PAD_DOWN:  {1, 1},
+	PAD_RIGHT: {1, 2},
+}
+
+var BUTTONS_DIRECTIONAL = map[Point]byte{}
+var BUTTONS_NUMERICAL = map[Point]byte{}
+
+func init() {
+
+	for key, val := range POS_NUMERICAL {
+		BUTTONS_NUMERICAL[val] = key
+	}
+
+	for key, val := range POS_DIRECTIONAL {
+		BUTTONS_DIRECTIONAL[val] = key
+	}
+}
+
 var dirs = map[byte]Point{
 	PAD_UP:    {-1, 0},
 	PAD_DOWN:  {+1, 0},
@@ -82,12 +104,8 @@ var dirs = map[byte]Point{
 	PAD_RIGHT: {0, +1},
 }
 
-var POS_DIRECTIONAL = map[byte]Point{
-	PAD_UP:    {0, 1},
-	PAD_A:     {0, 2},
-	PAD_LEFT:  {1, 0},
-	PAD_DOWN:  {1, 1},
-	PAD_RIGHT: {1, 2},
+type Edge struct {
+	begin, end byte
 }
 
 func calcMoves(dist Point) map[byte]int {
@@ -112,268 +130,157 @@ func calcMoves(dist Point) map[byte]int {
 	return mv
 }
 
-var (
-	ErrInvalidPosition   = fmt.Errorf("invalid position")
-	ErrBadDirection      = fmt.Errorf("bad direction")
-	ErrExplorationFailed = fmt.Errorf("exploration failed")
-)
-
-// **********************************************************************
-
-func solve(begin, button byte, pads []map[byte]Point) (string, error) {
-	if len(pads) == 0 {
-		return string(button), nil
-	}
-
-	pad := pads[0]
-	p0, isValidPadPosition := pad[begin]
-	if !isValidPadPosition {
-		return "", ErrInvalidPosition
-	}
-
-	p1, found := pad[button]
-	lib.MustBeTrue(found)
-
-	dist := PointSub(p1, p0)
-	moves := calcMoves(dist)
-
-	return explore(PAD_A, moves, pads[1:])
-}
-
-func explore(begin byte, moves map[byte]int, pads []map[byte]Point) (string, error) {
+func explorePaths(pos0 Point, moves map[byte]int, positions map[byte]Point, buttons map[Point]byte) [][]byte {
 
 	if len(moves) == 0 {
-		return solve(begin, PAD_A, pads)
+		// fmt.Println("END")
+		return make([][]byte, 1)
 	}
 
-	seq := ""
-	best := -1
+	paths := make([][]byte, 0)
 
-	for b := range dirs {
+loop:
+	for dir, vec := range dirs {
 
-		s, err := move(begin, b, moves, pads)
-		if err != nil {
-			// fmt.Println("DEBUG: ERR: ", err)
-			continue
+		// fmt.Println("EXPLORE", string(dir))
+
+		steps, found := moves[dir]
+		if !found {
+			// fmt.Println("CONTINUE: no move")
+			continue loop
 		}
-		// fmt.Println("DEBUG: NO ERR")
 
-		c := len(s)
-		isFirstValidSolution := best < 0
-		isBetterSolution := c < best
-		if isFirstValidSolution || isBetterSolution {
-			best = c
-			seq = s
-		}
-	}
-
-	if best < 0 {
-		return "", ErrExplorationFailed
-	}
-
-	return seq, nil
-}
-
-func move(begin, button byte, moves map[byte]int, pads []map[byte]Point) (string, error) {
-
-	steps, found := moves[button]
-	if !found || steps == 0 {
-		return "", ErrBadDirection
-	}
-
-	seq := ""
-
-	state := begin
-	for range steps {
-		s, err := solve(state, button, pads)
-		if err != nil {
-			return "", err
-		}
-		seq += s
-		state = button
-	}
-
-	delete(moves, button)
-	s, err := explore(state, moves, pads)
-	moves[button] = steps
-
-	return seq + s, err
-
-}
-
-func isValidPosition(pad map[byte]Point, pos Point) bool {
-	for _, p := range pad {
-		if p == pos {
-			return true
-		}
-	}
-	return false
-}
-
-func sim(seq string, robots []Point, pads []map[byte]Point) string {
-	if seq == "" {
-		return ""
-	}
-	return apply(seq[0], robots, pads) + sim(seq[1:], robots, pads)
-}
-
-func apply(button byte, robots []Point, pads []map[byte]Point) string {
-
-	if len(robots) == 0 {
-		return string(button)
-	}
-
-	if button == PAD_A {
-		for b, p := range pads[0] {
-			if p == robots[0] {
-				return apply(b, robots[1:], pads[1:])
+		pos1 := pos0
+		for range steps {
+			pos1 = PointAdd(pos1, vec)
+			if _, valid := buttons[pos1]; !valid {
+				// fmt.Println("CONTINUE: invalid position")
+				continue loop
 			}
 		}
-		panic("got here")
-	}
 
-	robots[0] = PointAdd(robots[0], dirs[button])
+		// fmt.Println("NEXT")
 
-	if !isValidPosition(pads[0], robots[0]) {
-		panic("invalid position")
-	}
+		delete(moves, dir)
+		ps := explorePaths(pos1, moves, positions, buttons)
+		moves[dir] = steps
 
-	return ""
-}
+		// fmt.Println("NEXT Paths", ps)
 
-// *** BRUTE-FORCE ***
-
-type State struct {
-	r0, r1, r2 Point
-	want       string
-}
-
-func (s *State) robots() []*Point {
-	return []*Point{&s.r0, &s.r1, &s.r2}
-}
-
-type Value struct {
-	sequ  string
-	valid bool
-}
-
-var memo = make(map[State]Value)
-
-func exploreBF(state State) Value {
-	if _, exists := memo[state]; !exists {
-		memo[state] = exploreBFImpl(state)
-	}
-	return memo[state]
-}
-
-var buttonsBruteForce = []byte{PAD_UP, PAD_DOWN, PAD_LEFT, PAD_RIGHT, PAD_A}
-
-func exploreBFImpl(state State) Value {
-
-	if state.want == "" {
-		return Value{"", true}
-	}
-
-	best := Value{"", false}
-
-	for _, button := range buttonsBruteForce {
-
-		val := applyBF(button, state)
-		if !val.valid {
-			continue
-		}
-
-		if !best.valid || len(val.sequ) < len(best.sequ) {
-			best = val
+		prev := bytes.Repeat([]byte{dir}, steps)
+		for _, next := range ps {
+			full := append(prev, next...)
+			paths = append(paths, full)
 		}
 	}
 
-	return best
+	return paths
 }
 
-var padsBruteForce = []map[byte]Point{POS_DIRECTIONAL, POS_DIRECTIONAL, POS_NUMERIC}
+func calcPaths(begin, end byte, positions map[byte]Point, buttons map[Point]byte) [][]byte {
+	if begin == end {
+		return make([][]byte, 1)
+	}
 
-func applyBF(button byte, state State) Value {
-
-	rs := state.robots()
-	out, valid := applyBFImpl(button, rs, padsBruteForce)
+	p0, valid := positions[begin]
 	if !valid {
-		return Value{"", false}
-	}
-	lib.MustBeTrue(len(out) < 2)
-
-	want := state.want
-	if len(out) == 1 {
-		lib.MustBeTrue(len(want) > 0)
-		if want[0] == out[0] {
-			want = want[1:]
-		}
+		return nil
 	}
 
-	s := State{
-		r0:   *rs[0],
-		r1:   *rs[1],
-		r2:   *rs[2],
-		want: want,
+	p1, valid := positions[end]
+	if !valid {
+		return nil
 	}
 
-	v := exploreBF(s)
-	v.sequ = v.sequ + string(button)
+	dist := PointSub(p1, p0)
 
-	return v
+	moves := calcMoves(dist)
+
+	// fmt.Printf("CALC : %s -> %s : %v\n", string(begin), string(end), dist)
+
+	return explorePaths(p0, moves, positions, buttons)
 }
 
-func applyBFImpl(button byte, robots []*Point, pads []map[byte]Point) (string, bool) {
+func makeMap(positions map[byte]Point, buttons map[Point]byte) map[Edge][][]byte {
 
-	if len(robots) == 0 {
-		return "", true
-	}
+	dict := make(map[Edge][][]byte)
 
-	if button == PAD_A {
-		for b, p := range pads[0] {
-			if p == *(robots[0]) {
-				return applyBFImpl(b, robots[1:], pads[1:])
-			}
+	for begin := range positions {
+		for end := range positions {
+			edge := Edge{begin: begin, end: end}
+
+			paths := calcPaths(begin, end, positions, buttons)
+			dict[edge] = paths
+
+			// fmt.Printf("EDGE %s -> %s\n", string(begin), string(end))
+			// for _, path := range paths {
+			// 	fmt.Println(" * ", string(path))
+			// }
 		}
 	}
 
-	*(robots[0]) = PointAdd(*(robots[0]), dirs[button])
+	return dict
+}
 
-	return "", isValidPosition(pads[0], *(robots[0]))
+func buildSeq(code []byte, maps []map[Edge][][]byte) string {
 
+	if len(maps) == 0 {
+		return string(code)
+	}
+
+	seq := ""
+
+	last := byte(PAD_A)
+	for _, next := range code {
+		best := "INITIAL"
+		paths := maps[0][Edge{last, next}]
+		for _, p := range paths {
+			s := buildSeq(append(p, PAD_A), maps[1:])
+			if len(s) < len(best) || best == "INITIAL" {
+				best = s
+			}
+		}
+
+		if best != "INITIAL" {
+			seq += best
+		}
+
+		last = next
+	}
+
+	return seq
 }
 
 func part1(in Input) {
 
+	mapDirpad := makeMap(POS_DIRECTIONAL, BUTTONS_DIRECTIONAL)
+
+	mapNumpad := makeMap(POS_NUMERICAL, BUTTONS_NUMERICAL)
+
+	// paths, found := mapNumpad[Edge{PAD_7, PAD_0}]
+	// lib.MustBeTrue(found)
+	// for _, p := range paths {
+	// 	fmt.Println("===>", string(p))
+	// }
+
+	// lib.MustPressEnter()
+
 	sol := 0
-
 	for _, code := range in.codes {
-
-		fmt.Printf("%s: ", string(code))
-		state := State{
-			r0:   padsBruteForce[0][PAD_A],
-			r1:   padsBruteForce[1][PAD_A],
-			r2:   padsBruteForce[2][PAD_A],
-			want: string(code),
-		}
-
-		val := exploreBF(state)
-		lib.MustBeTrue(val.valid)
-
-		seq := val.sequ
+		seq := buildSeq(code, []map[Edge][][]byte{mapNumpad, mapDirpad, mapDirpad})
+		fmt.Printf("CODE %s: %s\n", string(code), seq)
 
 		numericPart := lib.MustToInt(strings.ReplaceAll(string(code), "A", ""))
 		cost := len(seq)
 		sol += cost * numericPart
 
-		fmt.Printf("%s (%d * %d)\n", seq, cost, numericPart)
-
-		simRobots := make([]Point, len(padsBruteForce))
-		for i, pad := range padsBruteForce {
-			simRobots[i] = pad[PAD_A]
-		}
-		have := sim(seq, simRobots, padsBruteForce)
-		lib.MustBeEqual(string(code), have)
+		// // *** SIMULATION / VALIDATION ***
+		// simRobots := make([]Point, len(padsBruteForce))
+		// for i, pad := range padsBruteForce {
+		// 	simRobots[i] = pad[PAD_A]
+		// }
+		// have := sim(seq, simRobots, padsBruteForce)
+		// lib.MustBeEqual(string(code), have)
 		// lib.MustPressEnter()
 	}
 
@@ -384,3 +291,45 @@ func part2(in Input) {
 	sol := 0
 	fmt.Println("SOLUTION TO PART 2:", sol)
 }
+
+// var padsBruteForce = []map[byte]Point{POS_DIRECTIONAL, POS_DIRECTIONAL, POS_NUMERICAL}
+
+// func isValidPosition(pad map[byte]Point, pos Point) bool {
+// 	for _, p := range pad {
+// 		if p == pos {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// func sim(seq string, robots []Point, pads []map[byte]Point) string {
+// 	if seq == "" {
+// 		return ""
+// 	}
+// 	return apply(seq[0], robots, pads) + sim(seq[1:], robots, pads)
+// }
+
+// func apply(button byte, robots []Point, pads []map[byte]Point) string {
+
+// 	if len(robots) == 0 {
+// 		return string(button)
+// 	}
+
+// 	if button == PAD_A {
+// 		for b, p := range pads[0] {
+// 			if p == robots[0] {
+// 				return apply(b, robots[1:], pads[1:])
+// 			}
+// 		}
+// 		panic("got here")
+// 	}
+
+// 	robots[0] = PointAdd(robots[0], dirs[button])
+
+// 	if !isValidPosition(pads[0], robots[0]) {
+// 		panic("invalid position")
+// 	}
+
+// 	return ""
+// }
